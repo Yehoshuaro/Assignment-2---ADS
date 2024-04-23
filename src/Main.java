d1e85217801bc9ce81d820a79f8650600758e487
Yehoshuaro <231132@astanait.edu.kz>
2024-04-23 22:23:56 +0500

    Third Task on Java

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,34 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
 
-        int numElements = sc.nextInt();
-        int[] numbers = new int[numElements];
-        for (int i = 0; i < numElements; i++) {
-            numbers[i] = sc.nextInt();
-        }
-        int numQueries = sc.nextInt();
-        sc.nextLine();
+        int[] shelf = new int[2 * N];
+        int left = N - 1, right = N;
 
-        for (int i = 0; i < numQueries; i++) {
-            String queryType = sc.nextLine();
-            if (queryType.equals("Insert")) {
-                int index = sc.nextInt();
-                int value = sc.nextInt();
-                numbers = insert(numbers, index, value);
+        for (int i = 0; i < N; i++) {
+            int operation = scanner.nextInt();
+
+            if (operation == 1 || operation == 2){
+                int diskNumber = scanner.nextInt();
+                if (operation == 1) {
+                    shelf[left--] = diskNumber;
+                } else {
+                    shelf[right++] = diskNumber;
+                }
             }
-            else {
-                int removeIndex = sc.nextInt();
-                numbers = remove(numbers, removeIndex);
+            else{
+                if (operation == 3) {
+                    System.out.print(shelf[++left] + " ");
+                } else {
+                    System.out.print(shelf[--right] + " ");
+                }
             }
-            sc.nextLine();
         }
-        for (int num : numbers) {
-            System.out.print(num + " ");
-        }
-    }
-
-    public static int[] insert(int[] arr, int index, int value) throws Exception {
-        if (index < 0 || index > arr.length) {
-            throw new Exception("Invalid insertion index");
-        }
-
-        int[] newArr = new int[arr.length + 1];
-        System.arraycopy(arr, 0, newArr, 0, index);
-
-        newArr[index] = value;
-        System.arraycopy(arr, index, newArr, index + 1, arr.length - index);
-
-        return newArr;
-    }
-    public static int[] remove(int[] arr, int index) throws Exception {
-        if (index < 0 || index >= arr.length) {
-            throw new Exception("Invalid removal index");
-        }
-
-        int[] newArr = new int[arr.length - 1];
-        System.arraycopy(arr, 0, newArr, 0, index);
-        System.arraycopy(arr, index + 1, newArr, index, arr.length - index - 1);
-
-        return newArr;
+        scanner.close();
     }
 }
+
 
