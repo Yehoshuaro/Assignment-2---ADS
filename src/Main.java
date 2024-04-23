4a5b9ad69abb25bae26ad503e08c77d595af6ee5
Yehoshuaro <231132@astanait.edu.kz>
2024-04-23 23:50:37 +0500

    Sixth Task on Java

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,55 @@
+import java.util.Scanner;
+
 public class Main {
-    public static int WithoutSamsa(int[] studentPreferences, int[] samsas) {
-        int circularSamsaLovers = 0;
-        int squareSamsaLovers = 0;
-        int totalStudents = samsas.length;
-        for (int preference : studentPreferences) {
-            if (preference == 1) {
-                squareSamsaLovers++;
-            } else {
-                circularSamsaLovers++;
-            }
+    public static Integer[] maxHeapify(Integer[] array, Integer i) {
+        Integer leftChild = 2 * i + 1; // it's a left potok
+        Integer rightChild = 2 * i + 2; // this is right potok
+
+        Integer maxIndex = i;
+        if (leftChild < array.length && array[leftChild] > array[maxIndex])
+            maxIndex = leftChild;
+        if (rightChild < array.length && array[rightChild] > array[maxIndex])
+            maxIndex = rightChild;
+
+        if (maxIndex != i) {
+            Integer temp = array[i];
+            array[i] = array[maxIndex];
+            array[maxIndex] = temp;
+
+            array = maxHeapify(array, maxIndex);
         }
-        for (int i = 0; i < totalStudents; i++) {
-            if (samsas[i] == 0 && circularSamsaLovers > 0) {
-                circularSamsaLovers--;
-            }
-            else if (samsas[i] == 1 && squareSamsaLovers > 0) {
-                squareSamsaLovers--;
-            }
-            else {
-                return totalStudents - i;
-            }
+        return array;
+    }
+    public static Integer[] buildMaxHeap(Integer[] array) {
+        for (int i = array.length / 2; i >= 0; i--) {
+            array = maxHeapify(array, i);
         }
-        return 0;
+        return array;
     }
     public static void main(String[] args) {
-        int[] studentPreferences = {1, 1, 0, 0};
-        int[] samsas = {0, 0, 1, 0};
-        System.out.println(WithoutSamsa(studentPreferences, samsas));
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Size:");
+        int size = scanner.nextInt();
+        Integer[] initialArray = new Integer[size];
+
+        System.out.println("");
+        for (int i = 0; i < size; i++) {
+            initialArray[i] = scanner.nextInt();
+        }
+        Integer[] maxHeapArray = buildMaxHeap(initialArray);
+
+        System.out.println("Max Heap Array:");
+        for (Integer value : maxHeapArray) {
+            System.out.println(value);
+        }
+        System.out.println("\nHeap Structure:");
+        for (int i = 0; i < maxHeapArray.length / 2; i++) {
+            Integer parent = maxHeapArray[i];
+            Integer leftChild = (2 * i) + 1 < maxHeapArray.length ? maxHeapArray[(2 * i) + 1] : null;
+            Integer rightChild = (2 * i) + 2 < maxHeapArray.length ? maxHeapArray[(2 * i) + 2] : null;
+
+            System.out.println("Parent: " + parent + ", Left Child: " + leftChild + ", Right Child: " + rightChild);
+        }
     }
 }
-
-
